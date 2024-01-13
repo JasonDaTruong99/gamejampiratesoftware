0540f59db2092c1fd17f6c4fe43cd00b2d4da68c
JasonDaTruong <jasontruong@usf.edu>
2024-01-13 16:14:09 -0500

    movement
    added wasd player movement & animations

---
+++ player/mckun.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+@export var speed: int = 60
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+func movement():
+	var modeDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = modeDirection*speed
+	
+	if velocity.x < 0:
+		#animated_sprite_2d.flip_h
+		animated_sprite_2d.play('walkLeft')
+	elif velocity.x > 0:
+		animated_sprite_2d.play('walkRight')
+	elif velocity.y > 0:
+		animated_sprite_2d.play('walkDown')
+	elif velocity.y < 0:
+		animated_sprite_2d.play('walkUp')
+	else:
+		animated_sprite_2d.stop()
+		
+
+func _physics_process(delta):
+	movement()
+	move_and_slide()
