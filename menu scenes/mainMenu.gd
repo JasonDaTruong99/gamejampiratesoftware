09b725513f60c7f0ab50a6940d62a2e92ff370b6
JasonDaTruong <jasontruong@usf.edu>
2024-01-13 02:22:20 -0500

    assets added
    - added cave tilesets
    - added parallax backgrounds
    - gave main menu a background

--- menu scenes/mainMenu.gd
+++ menu scenes/mainMenu.gd
@@ -4,10 +4,8 @@ func _on_start_pressed():
 	#File path to tutorial on init will go here, then to saved progress afterwards
 	get_tree().change_scene_to_file("placeholder") 
 
-
 func _on_settings_pressed():
 	get_tree().change_scene_to_file("res://menu scenes/settings.tscn")
-
 
 func _on_quit_pressed():
 	get_tree().quit()
