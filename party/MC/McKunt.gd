25fad92287e7ba35637679b63cb8ff352e15fdfd
Jason <jason.brandwein@kwa-analytics.com>
2024-01-17 20:07:32 -0500

    Add MCKunt the enemy.

---
+++ party/MC/McKunt.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+var stats = preload("res://party/MC/McKunt.tres")
+
+const ACCEL: int = 0
+const MAX_SPEED: int = 0
+const FRICTION: int = 0
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var info = $CharData
+#this var was for when I was trying to fig out camera will need to get it working sometime
+#@onready var follow = $follow
+var level = stats.initLevel(1)
+var charStats: Dictionary = stats.passCharStats(5,5,5,5,100)
+var maxHealth = stats.calcMaxHealth(charStats["Body"], level)
+var maxLogos = stats.calcMaxLogos(charStats["Mind"], level)
+var currentHealth: int
+var currentLogos: int
+var xpRequired: int
+var xpCurrent: int
+var weapon: int #inventory/item sys?
+var damage = stats.calcAttackDamage(charStats["Body"], weapon)
+
+func _physics_process(delta):
+	var inputVector = Vector2.ZERO
+	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	inputVector = inputVector.normalized()
+	
+	if inputVector != Vector2.ZERO:
+		velocity = velocity.move_toward(inputVector * MAX_SPEED, ACCEL * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO,FRICTION * delta)
+	if velocity.x < 0:
+		#animated_sprite_2d.flip_h
+		animated_sprite_2d.play('walkLeft')
+	elif velocity.x > 0:
+		animated_sprite_2d.play('walkRight')
+	elif velocity.y > 0:
+		animated_sprite_2d.play('walkDown')
+	elif velocity.y < 0:
+		animated_sprite_2d.play('walkUp')
+	else:
+		animated_sprite_2d.stop()
+		
+	move_and_collide(velocity)
+
+#func _process(delta):
+	#follow.position = position
+
+
